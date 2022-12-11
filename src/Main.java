69afd6d99d97411b51dcf7a6fe284c38d638d63a
Alex <loalantref@hmail.com>
2022-12-11 15:20:22 +1200

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
+    private final static int CURRENTYEAR = LocalDate.now().getYear();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    printLeapOrNotLeapYear(CURRENTYEAR);
+    }
+
+    public static void printSeparator() {
+        System.out.println("+++++++++++++++++++++++++++++");
+    }
+
+    public static boolean isLeapYear (int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    public static void printLeapOrNotLeapYear(int year) {
+         if (isLeapYear(year)) {
+            System.out.println("Текуший " + year + " год является високосным.");
+        }else{
+            System.out.println("Текуший " + year + " год не является високосным.");
+        }
+        printSeparator();
+
+
+
+
+
+
     }
 }
