c9e9c7e8ef06f34f8e07b4ade9649eb36f97fd91
Alex <loalantref@hmail.com>
2022-12-11 16:02:03 +1200

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
+    private final static int CURRENTYEAR = LocalDate.now().getYear();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printLeapOrNotLeapYear(CURRENTYEAR);
+        printVersionOS(2018, 1);
     }
+
+    public static void printSeparator() {
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    }
+
+    public static boolean isLeapYear (int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    public static void printLeapOrNotLeapYear(int year) {
+        if (isLeapYear(year)) {
+            System.out.println("Текуший " + year + " год является високосным.");
+        }else{
+            System.out.println("Текуший " + year + " год не является високосным.");
+        }
+        printSeparator();
+    }
+
+    public static boolean isNowYear(int year) {
+        return year == CURRENTYEAR;
+    }
+    public static String getVersionOS(int versionOS){
+            if (versionOS == 0) {
+                return "IOS";
+            }
+        return "Android";
+        }
+
+    public static void printVersionOS(int year, int versionOS) {
+        if (isNowYear(year)) {
+            System.out.println("Установите полную версию приложения для " + getVersionOS(versionOS) + " по ссылке.");
+            } else {
+            System.out.println("Установите облегченную версию приложения для " + getVersionOS(versionOS) + " по ссылке.");
+            }
+            printSeparator();
+        }
+
+
+
 }
